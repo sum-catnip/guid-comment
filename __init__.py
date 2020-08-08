628e90a0e5ecf76f7090e97cd2c2073446605d0f
sum-catnip <catnip@catnip.fyi>
2020-08-08 16:27:15 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,34 @@
+from binaryninja.plugin import PluginCommand
+from binaryninja import BinaryView
+
+import struct
+
+
+def parse_part(data: bytes, fmt: str):
+    return format(struct.unpack(fmt, data)[0], 'x')
+
+
+def parse_guid(bv: BinaryView, addr: int):
+    data1 = parse_part(bv.read(addr, 4), '<L')
+    data2 = parse_part(bv.read(addr + 4, 2), '<H')
+    data3 = parse_part(bv.read(addr + 6, 2), '<H')
+    data4 = bv.read(addr + 8, 8).hex()
+    return f'{{{data1}-{data2}-{data3}-{data4[:4]}-{data4[4:]}}}'
+
+
+def comment_guids(bv: BinaryView):
+    for k, v in bv.data_vars.items():
+        if v.type.get_string_before_name() == 'GUID':
+            guid = parse_guid(bv, v.address)
+            existing = bv.get_comment_at(v.address)
+            if guid not in existing:
+                comment = existing or '' + guid
+                bv.set_comment_at(v.address, comment)
+            print(v)
+
+    return 'yeet'
+
+
+PluginCommand.register('GUID comment',
+                       'comment GUID instances in the proper format',
+                       comment_guids)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# GUID-comment
+adds a comment to every variable of type GUID containing the formatted GUID.
+to add the comments run the command `GUID comment`
+
+![screenshot](screen.png)
